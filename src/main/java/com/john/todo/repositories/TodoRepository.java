2be8c193b045b6985c935f8944dac0bd350052f0
JohnSilva98 <jonathan.design16@oulook.com>
2025-01-21 11:15:48 -0300

    creating list all open method to list all unfinished todos

--- src/main/java/com/john/todo/repositories/TodoRepository.java
+++ src/main/java/com/john/todo/repositories/TodoRepository.java
@@ -1,12 +1,18 @@
 package com.john.todo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.john.todo.domain.Todo;
 
 @Repository
 public interface TodoRepository  extends JpaRepository<Todo, Integer>{
+	
+	@Query("SELECT obj FROM Todo obj WHERE obj.finalizado = false ORDER BY obj.dataFinal")
+	List<Todo> findAllOpen();
 
 	
 }
--- src/main/java/com/john/todo/services/dbService.java
+++ src/main/java/com/john/todo/services/dbService.java
@@ -20,6 +20,12 @@ public class dbService {
 		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		Todo t1 = new Todo(null, "Estudar", "Estudar spring boot", LocalDateTime.parse("25/03/2022 10:40", format),
 				false);
-		todoRepository.saveAll(Arrays.asList(t1));
+		Todo t2 = new Todo(null, "Ler", "ler café com Deus pai", LocalDateTime.parse("22/05/2025 11:30", format),
+				true);
+		Todo t3 = new Todo(null, "Academia", "Treino ABC", LocalDateTime.parse("23/04/2023 09:00", format),
+				false);
+		Todo t4 = new Todo(null, "Estudar", "Estudar frontend", LocalDateTime.parse("25/03/2022 10:40", format),
+				true);
+		todoRepository.saveAll(Arrays.asList(t1,t2,t3,t4));
 	}
 }
