dee30b1ce03a6544016336ed0876d129d9c93e5d
JohnSilva98 <jonathan.design16@oulook.com>
2025-01-21 10:18:02 -0300

    creating id notations and generated value

--- src/main/java/com/john/todo/domain/Todo.java
+++ src/main/java/com/john/todo/domain/Todo.java
@@ -1,13 +1,21 @@
 package com.john.todo.domain;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-public class Todo {
+public class Todo implements Serializable{
 	
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String descricao;
