e082b2798f8d0b6290652e57d58901533feed9e3
JohnSilva98 <jonathan.design16@oulook.com>
2025-01-21 12:23:28 -0300

    creating custom exception for not found objects

---
+++ src/main/java/com/john/todo/resources/execeptions/StandartError.java
@@ -0,0 +1,49 @@
+package com.john.todo.resources.execeptions;
+
+import java.io.Serializable;
+
+public class StandartError implements Serializable{
+
+
+	private static final long serialVersionUID = 1L;
+	private long timestamp;
+	private Integer status;
+	private String message;
+
+	public StandartError() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StandartError(long timestamp, Integer status, String message) {
+		super();
+		this.timestamp = timestamp;
+		this.status = status;
+		this.message = message;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
