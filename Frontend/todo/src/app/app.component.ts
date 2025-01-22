5bcce0d4ec405e323d94a82496d2a86e4ed8dd87
JohnSilva98 <jonathan.design16@oulook.com>
2025-01-22 16:12:44 -0300

    creating header component and some basic css rules

--- Frontend/todo/src/app/app.component.ts
+++ Frontend/todo/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./components/header/header.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MatToolbarModule],
   templateUrl: "app.component.html",
 })
 export class AppComponent {
