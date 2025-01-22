b331822780bd9796cc42d63184a4f8885e7c6976
JohnSilva98 <jonathan.design16@oulook.com>
2025-01-22 14:42:34 -0300

    creating header component

--- Frontend/todo/src/app/app.component.ts
+++ Frontend/todo/src/app/app.component.ts
@@ -1,16 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  selector: "app-root",
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: "app.component.html",
 })
 export class AppComponent {
-  title = 'todo';
+  title = "todo";
 }
