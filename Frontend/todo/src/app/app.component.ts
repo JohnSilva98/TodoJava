e21232bb9d02111fb7298c03ccb15090a8957c19
JohnSilva98 <jonathan.design16@oulook.com>
2025-01-22 16:36:10 -0300

    creating footer component

--- Frontend/todo/src/app/app.component.ts
+++ Frontend/todo/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./components/header/header.component";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, HeaderComponent, MatToolbarModule],
+  imports: [RouterOutlet, HeaderComponent, MatToolbarModule, FooterComponent],
   templateUrl: "app.component.html",
 })
 export class AppComponent {
