205db5b313291404ecca79d9e3ad3b52760f7cda
JohnSilva98 <jonathan.design16@oulook.com>
2025-01-29 14:56:21 -0300

    creating the todo class to use in the frontend

--- Frontend/todo/src/app/components/read-all/read-all.component.ts
+++ Frontend/todo/src/app/components/read-all/read-all.component.ts
@@ -1,11 +1,28 @@
 import { Component } from "@angular/core";
 import { MatCard, MatCardHeader, MatCardModule } from "@angular/material/card";
 import { MatIcon } from "@angular/material/icon";
+import { Todo } from "../../models/todo";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-read-all",
-  imports: [MatCard, MatCardHeader, MatCardModule, MatIcon],
+  imports: [MatCard, MatCardHeader, MatCardModule, MatIcon, CommonModule],
   templateUrl: "./read-all.component.html",
   styleUrl: "./read-all.component.css",
 })
-export class ReadAllComponent {}
+export class ReadAllComponent {
+  list: Todo[] = [
+    {
+      title: "test",
+      description: "test",
+      deadline: new Date(),
+      isFinished: false,
+    },
+    {
+      title: "test2",
+      description: "test2",
+      deadline: new Date(),
+      isFinished: true,
+    },
+  ];
+}
