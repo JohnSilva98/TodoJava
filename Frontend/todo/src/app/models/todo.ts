cf3eaa88b0870f19306a03849c7109994c1af039
JohnSilva98 <jonathan.design16@oulook.com>
2025-02-11 15:58:03 -0300

    TRYINT TO CORRECT FETCH

--- Frontend/todo/src/app/models/todo.ts
+++ Frontend/todo/src/app/models/todo.ts
@@ -1,7 +1,7 @@
 export interface Todo {
   id?: String;
-  title: String;
-  description?: String;
-  deadline: Date;
-  isFinished: boolean;
+  titulo: String;
+  descricao?: String;
+  dataFinal: Date;
+  finalizado: boolean;
 }
